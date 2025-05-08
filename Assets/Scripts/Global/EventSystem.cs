864a8cd51824ed7985a70b380bce261ba6a6296a
NiekMSoftware <niek+git@meletstudios.com>
2025-05-08 21:18:31 +0200

    feat: OSM API calls
    OSM API calls werken ish

--- Assets/Scripts/Global/EventSystem.cs
+++ Assets/Scripts/Global/EventSystem.cs
@@ -1,12 +1,23 @@
 using System;
 using UnityEngine;
+using UnproductiveProductions.StadsBingo.GPS;
 
 namespace UnproductiveProductions.StadsBingo.Global
 {
     public class EventSystem : MonoBehaviour
     {
+        #region Events
+
+        // location events
         public event Action<double, double> OnLocationUpdated;
         public event Action OnLocationArrived;
+
+        // map events
+        public event Action<POI> OnPOISelected;
+        public event Action<Route> OnRouteChanged;
+        public event Action OnMapReady;
+
+        #endregion
 
         #region Singleton
 
@@ -27,14 +38,38 @@ namespace UnproductiveProductions.StadsBingo.Global
 
         #endregion
 
+        #region Event Invokers
+
         public void UpdateCoordinates(double lat, double lon)
         {
             OnLocationUpdated?.Invoke(lat, lon);
+            Debug.Log($"[{GetType().Name}] - Updated coordinates: {lat}, {lon}");
         }
 
         public void ArrivedAtLocation()
         {
             OnLocationArrived?.Invoke();
+            Debug.Log($"[{GetType().Name}] - Arrived at location.");
         }
+
+        public void SelectPOI(POI poi)
+        {
+            OnPOISelected?.Invoke(poi);
+            Debug.Log($"[{GetType().Name}] - Selected POI: {poi.Title}");
+        }
+
+        public void ChangeRoute(Route route)
+        {
+            OnRouteChanged?.Invoke(route);
+            Debug.Log($"[{GetType().Name}] - Changed route to: {route.RouteName}");
+        }
+
+        public void MapReady()
+        {
+            OnMapReady?.Invoke();
+            Debug.Log($"[{GetType().Name}] - Map is ready.");
+        }
+
+        #endregion
     }
 }
